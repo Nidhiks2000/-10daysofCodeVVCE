44a98b664bdf500a92edf3bb2045b11b8134c087
Nidhiks2000 <68379528+Nidhiks2000@users.noreply.github.com>
2020-08-20 06:09:59 +0000

    Update Equal Stacks.java

--- Equal Stacks.java
+++ Equal Stacks.java
@@ -12,9 +12,9 @@ public class Solution {
     static int equalStacks(int[] ha1, int[] ha2, int[] ha3) {
     
     // initialising stack of cylinders
-         Stack<Integer> s1 = new Stack<Integer>(); 
-        Stack<Integer> s2 = new Stack<Integer>(); 
-        Stack<Integer> s3 = new Stack<Integer>(); 
+         Stack<Integer> sta1 = new Stack<Integer>(); 
+        Stack<Integer> sta2 = new Stack<Integer>(); 
+        Stack<Integer> sta3 = new Stack<Integer>(); 
  
         int h1 = 0;   //Height of the first stack
         int h2 = 0;     //Height of the second stack
@@ -24,17 +24,17 @@ public class Solution {
  
         //Initialize the stacks and their heights
         for(int i = ha1.length-1; i >= 0 ; i--){
-            s1.push(ha1[i]);
+            sta1.push(ha1[i]);
             h1 += ha1[i];
         }
  
         for(int i = ha2.length-1; i >= 0 ; i--){
-            s2.push(ha2[i]);
+            sta2.push(ha2[i]);
             h2 += ha2[i];
         }
  
         for(int i = ha3.length-1; i >= 0 ; i--){
-            s3.push(ha3[i]);
+            sta3.push(ha3[i]);
             h3 += ha3[i];
         }
  
@@ -44,21 +44,21 @@ public class Solution {
         while(h1 != h2 || h1 != h3) {
             //Remove cylinders from stack 1 until your height is <= the smallest
             while(h1 > minStack){
-                h1 -= s1.pop();
+                h1 -= sta1.pop();
             }
             minStack = Math.min(h1,Math.min(h2,h3)); //Recalculate min
  
             //Remove cylinders from stack 2 until your height is <= the smallest
             while(h2 > minStack){
-                h2 -= s2.pop();
+                h2 -= sta2.pop();
             }
-            minStack = Math.min(h1,Math.min(h2,h3)); //Recalculate min
+            minStack = Math.min(h1,Math.min(h2,h3)); //Calculate min
  
             //Remove cylinders from stack 3 until your height is <= the smallest
             while(h3 > minStack){
-                h3 -= s3.pop();
+                h3 -= sta3.pop();
             }
-            minStack = Math.min(h1,Math.min(h2,h3)); //Recalculate min
+            minStack = Math.min(h1,Math.min(h2,h3));     //calculate min
  
         }
  
